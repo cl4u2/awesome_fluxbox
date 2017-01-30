d48c163745b3a26ec9a912e7dae4ea53e7f12580
Claudio Pisa <clauz@ninux.org>
2017-01-30 11:00:19 +0100

    do not add titlebars

--- rc.lua
+++ rc.lua
@@ -683,11 +683,6 @@ awful.rules.rules = {
         }
       }, properties = { floating = true }},
 
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
-    },
-
 	{ rule = { class = "Gkrellm" },
 	  properties = { floating = true, 
 	  				 sticky = true, 
@@ -703,6 +698,11 @@ awful.rules.rules = {
 					 skip_taskbar = true
 			       } 
 	},
+    -- Add titlebars to normal clients and dialogs
+    --{ rule_any = {type = { "normal", "dialog" }
+    --  }, properties = { titlebars_enabled = true }
+    --},
+
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
